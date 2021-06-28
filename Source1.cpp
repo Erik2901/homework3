a4504e8f4f77922f0067d0ae6d21b68a5db9ab8a
Erik2901 <erik2901@mail.ru>
2021-06-29 03:45:17 +0400

    hw3

---
+++ Source1.cpp
@@ -0,0 +1,5 @@
+extern const int a = 100;
+extern const int b = 60;
+extern const int c = 50;
+extern const int d = 30;
+double dv = 0.0;
---
+++ homework3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+// Задание 1,4
+extern const int a;
+extern const int b;
+extern const int c;
+extern const int d;
+extern double dv;
+//
+
+int main()
+{
+    // Задание 1,4
+    dv = a*(b + (static_cast<double>(c) / d));
+    std::cout << dv << std::endl;
+    //
+    // Задание 2
+    int q = 50;
+    int w = (q <= 21) ? q - 21 : q - 21 << 1;
+    std::cout << w << std::endl;
+    //
+    // Задание 3
+    int arrm[3][3][3];
+    int* pm = &arrm[1][1][1];
+    *pm = 15;
+    std::cout << *pm << std::endl;
+    //
+    system("pause");
+    return 0;
+}
+
